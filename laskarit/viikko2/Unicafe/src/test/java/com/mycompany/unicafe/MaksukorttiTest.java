cadacb8a5231ac57332c9d9679b535ac32250827
Lassi Laiho <lassi.laiho0@gmail.com>
2020-03-16 22:26:45 +0200

    Add more tests week 2

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,44 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+
+    @Test
+    public void latausKasvattaaSaldoa() {
+        kortti.lataaRahaa(15);
+        assertEquals(25, kortti.saldo());
+    }
+
+    @Test
+    public void ottoVahentaaSaldoaJosRahaaRiittavasti() {
+        kortti.otaRahaa(5);
+        assertEquals(5, kortti.saldo());
+    }
+
+    @Test
+    public void ottoEiMuutaSaldoaJosRahaaLiianVahan() {
+        kortti.otaRahaa(13);
+        assertEquals(10, kortti.saldo());
+    }
+
+    @Test
+    public void ottoPalauttaaTrueJosRahaaRiittaa() {
+        assertEquals(true, kortti.otaRahaa(5));
+    }
+
+    @Test
+    public void ottoPalauttaaFalseJosRahaEiRiita() {
+        assertEquals(false, kortti.otaRahaa(13));
+    }
+
+    @Test
+    public void toStringToimii() {
+        assertEquals("saldo: 0.10", kortti.toString());
     }
 }
