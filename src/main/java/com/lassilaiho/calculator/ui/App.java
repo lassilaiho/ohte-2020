fe1f0caf3dbe56d8509767da42634b6afe3b4ffc
Lassi Laiho <lassi.laiho0@gmail.com>
2020-03-21 13:12:09 +0200

    Remove unused code

--- src/main/java/com/lassilaiho/calculator/ui/App.java
+++ src/main/java/com/lassilaiho/calculator/ui/App.java
@@ -28,10 +28,6 @@ public class App extends Application {
         dbConnection.close();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
